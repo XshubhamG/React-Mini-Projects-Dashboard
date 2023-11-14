da27a8e027a46012ac2430ab8066adb23c1feecc
Shubham Giri <shubhammgiri3@gmail.com>
2023-11-14 22:37:12 +0530

    07 slider added

--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,7 @@ import img2 from '../images/02-Tours.png';
 import img3 from '../images/03-Reviews.png';
 import img4 from '../images/04-Accordians.png';
 import img5 from '../images/05-Menu.png';
+import img7 from "../images/07-Slider.png";
 
 
 const projects = [
@@ -35,6 +36,12 @@ const projects = [
         img: img5,
         url: "https://05-project-react.surge.sh",
         title: "Menu",
+    },
+    {
+        id: '07',
+        img: img7,
+        url: "https://07-project-react.surge.sh",
+        title: "Slider",
     },
 ]
 
